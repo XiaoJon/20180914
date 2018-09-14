6c0759d8abd898f1f5dba77eee45cbc3d218b2e0
xiaoJon <xiaojun6909@gmail.com>
2018-09-14 12:10:27 +0800

    del

---
+++ 关闭进程删除文件.bat
@@ -0,0 +1,3 @@
+taskkill /im java.exe /f
+del C:\WorkSpace\branches\development\TeamFace\common-ui\build\intermediates\bundles\default\libs\*.jar  /f/s/q/a 
+del C:\WorkSpace\branches\development\TeamFace\im\build\intermediates\bundles\default\libs\*.jar  /f/s/q/a 
